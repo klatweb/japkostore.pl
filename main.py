6528e04dc8cf4a781f507365e99afcd02d5611d7
qqkar <lewko5@wp.pl>
2023-05-29 20:45:43 +0200

    Modify

---
+++ main.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
