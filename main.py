539b48b2bc78e3162c9385fe7a65da026cb6b158
qqkar <lewko5@wp.pl>
2023-05-30 19:54:59 +0200

    Modify

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+
+driver.get("https://japkostore.pl/")
+
+my_account_link = driver.find_element(By.XPATH, "//div[@class='pk-myaccount']")
+my_account_link.click()
+
+my_account_button = driver.find_element(By.XPATH, "//div[@class='tab-content']/a")
+time.sleep(2)
+my_account_button.click()
