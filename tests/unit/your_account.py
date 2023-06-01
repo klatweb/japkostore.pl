9e213db3ccc24baaa4577c4edd45dc96b77b164c
qqkar <lewko5@wp.pl>
2023-06-01 20:33:53 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ tests/unit/your_account.py
@@ -0,0 +1,49 @@
+import time
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+
+
+class TestYourAccountPage:
+
+    @pytest.mark.positive
+    def test_information_page(self):
+        driver.get("https://japkostore.pl/")
+
+        my_account_link = driver.find_element(By.XPATH, "//div[@class='pk-myaccount']")
+        my_account_link.click()
+
+        my_account_button = driver.find_element(By.XPATH, "//div[@class='tab-content']/a")
+        time.sleep(2)
+        my_account_button.click()
+
+        email_input = driver.find_element(By.XPATH, "//div[@class='form-group row']/div[1]/div[1]/div[1]/div[1]/input["
+                                                    "@name='email']")
+        email_input.send_keys("stevenberry84@op.pl")
+
+        password_input = driver.find_element(By.XPATH, "//div[@class='form-group row']/div[1]/div[1]/div[1]/div["
+                                                       "1]/input[@name='password']")
+        password_input.send_keys("g238rIJIudkjabj@19Njnja")
+
+        login_button = driver.find_element(By.XPATH, "//button[@class='btn btn-primary form-control-submit']")
+        login_button.click()
+
+        information_link = driver.find_element(By.XPATH, "//div[@class='links']/a[@id='identity-link']")
+        information_link.click()
+
+        information_page_title = driver.title
+        expected_information_page_title = "Dane osobiste"
+        assert information_page_title == expected_information_page_title, "Incorrect page title. Expected: {" \
+                                                                          "expected_information_page_title}, " \
+                                                                          "actual: {driver.title}"
+
+
+
+
