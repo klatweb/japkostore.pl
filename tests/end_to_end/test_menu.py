62f8cf14fbdb5d5f930661f9c5af975f6a7d4fac
qqkar <4lewkowicz@gmail.com>
2023-06-11 07:30:57 +0200

    Update

---
+++ tests/end_to_end/test_menu.py
@@ -0,0 +1,63 @@
+import time
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+options.add_argument("disable-blink-features=AutomationControlled")
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+
+
+class TestMenuPage:
+
+    @pytest.mark.positive
+    @pytest.mark.menu_page
+    def test_iphone_case_page(self):
+        driver.get("https://japkostore.pl/")
+
+        my_account_link = driver.find_element(By.XPATH, "//div[@class='pk-myaccount']")
+        my_account_link.click()
+
+        my_account_button = driver.find_element(By.XPATH, "//div[@class='tab-content']/a")
+        time.sleep(2)
+        my_account_button.click()
+
+        email_input = driver.find_element(By.XPATH, "//div[@class='form-group row']/div[1]/div[1]/div[1]/div[1]/input["
+                                                    "@name='email']")
+        email_input.send_keys("stevenberry84@op.pl")
+
+        password_input = driver.find_element(By.XPATH, "//div[@class='form-group row']/div[1]/div[1]/div[1]/div["
+                                                       "1]/input[@name='password']")
+        password_input.send_keys("g238rIJIudkjabj@19Njnja")
+
+        login_button = driver.find_element(By.XPATH, "//button[@class='btn btn-primary form-control-submit']")
+        login_button.click()
+
+        menu_link = driver.find_element(By.XPATH, "/html//header[@id='header']/div["
+                                                  "@class='mobile-header-wrapper']/div[@class='elementor "
+                                                  "elementor-43010000 elementor-bc-flex-widget']/div["
+                                                  "@class='elementor-inner']/div/section[2]/div/div["
+                                                  "@class='elementor-row']/div//div[@class='elementor-element "
+                                                  "elementor-element-42fc8996 elementor-widget "
+                                                  "elementor-widget-pkmenu']//span[.='Menu']")
+
+        menu_link.click()
+
+        main_page_link = driver.find_element(By.XPATH, "//ul[@id='top-menu']//a["
+                                                       "@href='https://japkostore.pl/2-strona-glowna']/span")
+
+        main_page_link.click()
+
+        top_menu_iphone_hover = driver.find_element(By.XPATH, "//ul[@id='top-menu']/li[2]/a[@href='#']/span[1]")
+        ActionChains(driver).move_to_element(top_menu_iphone_hover)
+
+        categories_txt = driver.find_element(By.XPATH, "//*[@id='main-content']/div[2]/div/div/section/div/div/div["
+                                                       "1]/div/div/section[1]/div/div/div/div/div/div[1]/div/h2")
+        assert categories_txt.is_displayed()
+
+        driver.delete_all_cookies()
